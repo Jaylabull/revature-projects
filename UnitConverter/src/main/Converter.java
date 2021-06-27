07a275ab882224c091d46acc8901d17e37155b77
Jayla Bullard <jaylabullard@Jaylas-Air.attlocal.net>
2021-06-27 14:57:25 -0400

    Working methods for conversions added

--- UnitConverter/src/main/Converter.java
+++ UnitConverter/src/main/Converter.java
@@ -3,7 +3,7 @@ package main;
 import java.util.Scanner;
 
 public class Converter {
-	
+	static Scanner newScan = new Scanner(System.in);
 	public static void main(String[] args) {
 		
 		int menuSelection = 0;
@@ -16,7 +16,7 @@ public class Converter {
 		}
 		while(menuSelection != 0 );
 		
-		Scanner newScan = new Scanner(System.in);
+
 		System.out.println("What would you like to convert?");
 		String userSelection = newScan.nextLine();
 		
@@ -25,21 +25,46 @@ public class Converter {
 		switch(userSelection) {
 		case "1":
 			System.out.println("You selected Ounces to Cups");
+			System.out.println("Enter number to convert: ");
+			ounceToCup();
 			break;
 		case "2":
 			System.out.println("You selected Feet to Meters");
+			System.out.println("Enter number to convert: ");
+			feetToMeters();
 			break;
 		case "3":
 			System.out.println("You selected Miles to Kilometers");
+			System.out.println("Enter number to convert: ");
+			milesToKilometers();
 			break;
 		default:
 			System.out.println("This is not an option.");
 			break;
-		}//End While do loop
-		
-		System.out.println("Please enter what you would like to convert: ");
+		}//End Do-while loop
 			
 		
 	}//End Main
+	
+	public static void ounceToCup() {
+		Scanner scanDbl = new Scanner(System.in);
+		double userDbl = scanDbl.nextDouble();
+		System.out.print(userDbl/8);
+	
+	}
+	
+	public static void feetToMeters() {
+		Scanner scanDbl = new Scanner(System.in);
+		double userDbl = scanDbl.nextDouble();
+		System.out.print(userDbl/3.281);
+	
+	}
+	
+	public static void milesToKilometers() {
+		Scanner scanDbl = new Scanner(System.in);
+		double userDbl = scanDbl.nextDouble();
+		System.out.print(userDbl * 1.609);
+	
+	}
 	
 }//End Converter Class
